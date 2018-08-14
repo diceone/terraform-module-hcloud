4b52176b37679b67cec524542046356444795f92
Michael Vogeler <michael.vogeler@nexinto.com>
2018-08-14 13:30:41 +0200

    more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "k8sserver-ip" {
   value = ["${hcloud_server.k8sserver.ipv4_address}"]
 }
+
+output "k8s-all" {
+  value = ["${hcloud_server.k8s-all.*.ipv4_address}"]
+}
+
+output "k8s-etcd" {
+  value = ["${hcloud_server.k8s-etcd.*.ipv4_address}"]
+}
+
+output "k8s-controlplane" {
+  value = ["${hcloud_server.k8s-controlplane.*.ipv4_address}"]
+}
+
+output "k8s-worker" {
+  value = ["${hcloud_server.k8s-worker.*.ipv4_address}"]
+}
